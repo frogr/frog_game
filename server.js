c998a994b756c555b7e27f525b898713487a8b80
frogr <austindanielfrench@gmail.com>
2018-09-09 14:29:44 -0700

    basic boilerplate socket.io code working

--- server.js
+++ server.js
@@ -10,3 +10,54 @@ app.use(express.static(path.join(__dirname, 'public')));
 server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
+
+let userCount = 0;
+
+io.on('connection', socket => {
+  let newUser = false;
+
+  socket.on('new message', data => {
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  socket.on('add user', username => {
+    if (newUser) return;
+
+    socket.username = username;
+    userCount++;
+    newUser = true;
+    socket.emit('login', {
+      userCount: userCount
+    });
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      userCount: userCount
+    });
+  });
+
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('disconnect', () => {
+    if (newUser) {
+      userCount++;
+
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        userCount: userCount
+      });
+    }
+  });
+});
